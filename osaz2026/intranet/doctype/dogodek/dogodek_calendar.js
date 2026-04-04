5533bd24aeb0efc26e7f11ca6381f04dc831eacb
sp9leo <sp9leonardo@gmail.com>
2026-04-04 13:31:10 +0000

    added predvideno field

---
+++ osaz2026/intranet/doctype/dogodek/dogodek_calendar.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2026, osaz and contributors
+// For license information, please see license.txt
+
+frappe.views.calendar["Dogodek"] = {
+    // field_map: {
+	// 	start: "start_date",
+	// 	end: "end_date",
+	// },
+    // gantt: true
+};
